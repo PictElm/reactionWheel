610d723dcae1ba63c74a6a42ecf75cc5408119d0
FlorianC31 <72257044+FlorianC31@users.noreply.github.com>
2022-01-06 12:28:37 +0100

    Modif from Xavier & Florian

--- WheelTasks.c
+++ WheelTasks.c
@@ -8,6 +8,7 @@
 // ***********************************************************
 
 #include "ReactionWheel.h"
+#include
 
 //--------------------------------------------
 // Global  declarations                      
@@ -15,12 +16,28 @@
 
 #define HUMAN_MACHINE_INTERFACE_PRIORITY     33
 
+// Ajouté par Xavier & Florian
+// TODO: Priorities to be defined
+#define TIME_MANAGEMENT_PRIORITY     33
+#define CURVES_ACQUISITION_PRIORITY     33
+#define LAW_FUNCTION_PRIORITY     33
+#define TIME_MANAGEMENT_QUEUE_BUFFER_SIZE 4
+#define AP_FLAG 0
+#define LP_FLAG 1
+#define I_FLAG 2
+#define F_FLAG 3
+
 
 //--------------------------------------------
 // Tasks descriptors                          
 //--------------------------------------------
 
 RT_TASK HumanMachineInterface_TaskDescriptor;
+// Ajouté par Xavier & Florian
+RT_TASK TimeManagement_TaskDescriptor;
+RT_TASK CurvesAcquisition_TaskDescriptor;
+RT_TASK LawFunction_TaskDescriptor;
+RT_TASK Timer1ms_TaskDescriptor;
 
 
 //-------------------------------------------
@@ -31,6 +48,9 @@ RT_TASK HumanMachineInterface_TaskDescriptor;
 
 RT_SEM StartExperiment_Semaphore;
 RT_SEM ExitApplication_Semaphore;
+
+// Ajouté par Xavier & Florian
+RT_SEM Timer1ms_Semaphore;
 
 //--------------------------------------------
 //  Events identifiers
@@ -51,14 +71,24 @@ RT_SEM ExitApplication_Semaphore;
 //--------------------------------------------
 // Message queues Identifiers                 
 //--------------------------------------------
-RT_QUEUE  SensorsMeasurement_Queue;
+// Ajouté par Xavier & Florian
+RT_QUEUE  queue_TimeManagement;                
 
 
 // Global variables communication and synchronization tasks by shared memory 
 //--------------------------------------------------------------------------
-static bool AbortExperiment_Flag;
+// bool myFlag
+//**** This space must be completed if needed *****  
+static bool Finished_Flag;
 
-bool ExperimentIsAborted(void) { return AbortExperiment_Flag; }
+bool ConnectionIsFinished(void)
+{
+    return(Finished_Flag);
+}
+
+
+// declared in WheelHMI.c 
+extern ExperimentParametersType ExperimentParameters;
 
 // **************************************************************************
 // *
@@ -72,6 +102,69 @@ void HumanMachineInterface_Task()
     ManageRequest();
 }
 
+// Ajouté par Xavier & Florian
+void TimeManagement_Task()
+{
+    rt_printf("Starting Time Management task\r\n");
+
+	char AP_flag = AF_FLAG;
+	char LP_flag = LP_FLAG;
+	char I_flag = I_FLAG;
+	char F_flag = F_FLAG;
+
+    Finished_Flag = false;
+
+    while(true) { 
+		
+	
+        rt_sem_p(&StartExperiment_Semaphore, TM_INFINITE);
+
+        int t=0; 
+        //Push I to queue
+		rt_queue_write(&queue_TimeManagement, &I_flag, sizeof(char), Q_NORMAL);
+
+        while(ConnectionIsActive() && t < ExperimentParameters.duration && !ExperimentIsAborted()) {
+			// Attente du semaphore du timer 1ms
+			rt_sem_p(&Timer1ms_Semaphore, 2000000); //timeout = 2ms
+			
+            t++;
+			
+            if (t % ExperimentParameters.lawPeriod == 0){ 
+                //Push LP to queue
+				rt_queue_write(&queue_TimeManagement, &LP_flag, sizeof(char), Q_NORMAL);
+            } 
+
+            if (t % ExperimentParameters.acquisitionPeriod == 0){ 
+                //Push AP to queue 
+				rt_queue_write(&queue_TimeManagement, &AP_flag, sizeof(char), Q_NORMAL);
+            } 
+
+        } 
+		
+        Finished_Flag = true;
+        // Push F to queue 
+		rt_queue_write(&queue_TimeManagement, &F_flag, sizeof(char), Q_NORMAL);
+    } 
+}
+
+void Timer1ms_Task(){
+	rt_sem_v(&Timer1ms_Semaphore);	
+}
+
+// Ajouté par Xavier & Florian
+void LawFunction_Task()
+{
+
+
+}
+
+// Ajouté par Xavier & Florian
+void CurvesAcquisition_Task()
+{
+
+
+}
+
 
 //-----------------------------------------------------------
 // functions called from HMI_Utilities
@@ -81,13 +174,13 @@ void HumanMachineInterface_Task()
  */
 void AbortExperiment(void)
 {
-   AbortExperiment_Flag = true;
+   
 }
 //-----------------------------------------------------------
 
 void StartExperiment(void)
 {
-   rt_sem_v(&StartExperiment_Semaphore);
+   
 }
 //----------------------------------------------------------
 
@@ -95,19 +188,19 @@ void ReturnSensorsMeasurement()
 {
     float samplesBlock[50 * 4];
     char terminationChar;
-    int i = 0;
+    int i;
     
-    // read from the queue
-    // write into samplesBlock at the right position
-    // increment i (for next iteration's position)
-    // don't wait for message
-    // if no messages, read returns EWOULDBLOCK, so the loop stops
-    while(rt_queue_read(&SensorsMeasurement_Queue, samplesBlock + (i*4), 4*sizeof(float), TM_NONBLOCK) != EWOULDBLOCK)
-        i++;
+    for(i = 0; i < 10; i++) {
+        samplesBlock[(i * 4)]     = 0.1;
+        samplesBlock[(i * 4) + 1] = 0.2;
+        samplesBlock[(i * 4) + 2] = 0.3;
+        samplesBlock[(i * 4) + 3] = 0.4;
+    }
    
-    terminationChar = ExperimentIsFinished() ? 'S' : 'F';
+    terminationChar = 'S';  // terminationChar = 'F' if the experiment is finished
 
-    WriteRealArray(terminationChar, samplesBlock, i * 4);
+    WriteRealArray(terminationChar, samplesBlock, 10 * 4);
+
 }
 
 //--------------------------------------------------------------------
@@ -149,13 +242,16 @@ int main(int argc, char* argv[])
     
     // Message queues creation               
     // ------------------------------------- 
-    // TODO: decide on BUFFER_SIZE (like 50 or whever - see ReturnSenorsMeasurment)
-    rt_queue_create(&SensorsMeasurement_Queue, "SensorsMeasurement_Queue", 50 * 4*sizeof(float), 50, Q_FIFO);
+    // *** This space must be completed  if needed   ******
+
+    rt_queue_create(&queue_TimeManagement, "Queue TimeManagement", TIME_MANAGEMENT_QUEUE_BUFFER_SIZE * sizeof(char), TIME_MANAGEMENT_QUEUE_BUFFER_SIZE, Q_FIFO);
 
     // Semaphores creation                 
     // ------------------------------------
     rt_sem_create(&ExitApplication_Semaphore, "Exit", 0, S_FIFO);
-    rt_sem_create(&StartExperiment_Semaphore, "StartExperiment", 0, S_PULSE);
+
+	// Ajouté par Florian & Xavier
+    rt_sem_create(&Timer1ms_Semaphore);
     // **** This space must be completed  if needed   ***** 
     
 
@@ -170,8 +266,24 @@ int main(int argc, char* argv[])
     
     rt_task_create(&HumanMachineInterface_TaskDescriptor, "ManageRequestTask", DEFAULTSTACKSIZE, HUMAN_MACHINE_INTERFACE_PRIORITY, 0);
 
+
+    // Ajouté par Florian & Xavier,
+	// TODO: mode=0 à modifier et à faire valider
+    rt_task_create(&TimeManagement_TaskDescriptor, "TimeManagement", DEFAULTSTACKSIZE, TIME_MANAGEMENT_PRIORITY, 0);
+    rt_task_create(&CurvesAcquisition_TaskDescriptor, "CurvesAcquisition", DEFAULTSTACKSIZE, CURVES_ACQUISITION_PRIORITY, 0);
+    rt_task_create(&LawFunction_TaskDescriptor, "LawFunction", DEFAULTSTACKSIZE, LAW_FUNCTION_PRIORITY, 0);
+    rt_task_create(&Timer1ms_TaskDescriptor, "Timer1ms", DEFAULTSTACKSIZE, LAW_FUNCTION_PRIORITY, 0);
+
+	
     // Tasks starting
     rt_task_start(&HumanMachineInterface_TaskDescriptor, &HumanMachineInterface_Task, NULL);
+
+    // Ajouté par Florian & Xavier
+	rt_task_set_periodic(&Timer1ms_TaskDescriptor,TM_NOW,1000000); // activation each 1ms
+    rt_task_start(&Timer1ms_TaskDescriptor, &Timer1ms_Task, NULL);
+    rt_task_start(&TimeManagement_TaskDescriptor, &TimeManagement_Task, NULL);
+    rt_task_start(&CurvesAcquisition_TaskDescriptor, &CurvesAcquisition_Task, NULL);
+    rt_task_start(&LawFunction_TaskDescriptor, &LawFunction_Task, NULL);
 
     //-----------------------------------------------------------
     // Main Task waits on exit semaphore                          
@@ -185,6 +297,12 @@ int main(int argc, char* argv[])
     rt_printf("\r\n \r\n Destruction of \r\n  - Tasks\r\n  - Queues\r\n  - Semaphores\r\n  - Events\r\n  - Mutexes\r\n");
     // **** This space must be completed *****  
     rt_task_delete(&HumanMachineInterface_TaskDescriptor);
+
+    // Ajouté par Florian & Xavier
+    rt_task_delete(&Timer1ms_TaskDescriptor);	
+    rt_task_delete(&TimeManagement_TaskDescriptor);
+    rt_task_delete(&CurvesAcquisition_TaskDescriptor);
+    rt_task_delete(&LawFunction_TaskDescriptor);
     
 
     //------------------------------------------------------------
@@ -194,7 +312,9 @@ int main(int argc, char* argv[])
     //   **** This space must be completed  if needed   ***** 
 
     rt_sem_delete(&ExitApplication_Semaphore);
-    rt_sem_delete(&StartExperiment_Semaphore);
+	
+	// Ajouté par Florian & Xavier
+    rt_sem_delete(&Timer1ms_Semaphore);
 
     //------------------------------------------------------------
     // Events destruction                                 
@@ -206,8 +326,11 @@ int main(int argc, char* argv[])
     //------------------------------------------------------------
     // Message queues destruction                                 
     //------------------------------------------------------------
-    rt_queue_delete(&SensorsMeasurement_Queue);
-    
+    // rt_queue_delete(&myQueue);                                 
+    // **** This space must be completed  if needed   *****
+	
+	// Ajouté par Florian & Xavier
+    rt_queue_delete(&queue_TimeManagement);
 
     rt_printf(" Application ..... finished--> exit\r\n");
     // Peripherals uninitialization 
