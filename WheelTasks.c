1683f0af4f2bc853a5fac9d4746542983d08fe84
FlorianC31 <72257044+FlorianC31@users.noreply.github.com>
2022-01-11 12:37:42 +0100

    Time Management OK (TP 11 janvier)

--- WheelTasks.c
+++ WheelTasks.c
@@ -14,13 +14,11 @@
 //--------------------------------------------
 
 #define HUMAN_MACHINE_INTERFACE_PRIORITY     33
+#define TIME_MANAGEMENT_PRIORITY             42
+#define ACQUIRE_AND_LAW_PRIORITY             10
 
-// Ajouté par Xavier & Florian
-// TODO: Priorities to be defined
-#define TIME_MANAGEMENT_PRIORITY     33
-#define CURVES_ACQUISITION_PRIORITY     33
-#define LAW_FUNCTION_PRIORITY     33
 #define TIME_MANAGEMENT_QUEUE_BUFFER_SIZE 4
+
 #define AP_FLAG 0
 #define LP_FLAG 1
 #define I_FLAG 2
@@ -33,9 +31,7 @@
 RT_TASK HumanMachineInterface_TaskDescriptor;
 // Ajouté par Xavier & Florian
 RT_TASK TimeManagement_TaskDescriptor;
-RT_TASK CurvesAcquisition_TaskDescriptor;
-RT_TASK LawFunction_TaskDescriptor;
-RT_TASK Timer1ms_TaskDescriptor;
+RT_TASK AcquireAndLaw_TaskDescriptor;
 
 
 //-------------------------------------------
@@ -46,7 +42,7 @@ RT_TASK Timer1ms_TaskDescriptor;
 
 RT_SEM StartExperiment_Semaphore;
 RT_SEM ExitApplication_Semaphore;
-RT_SEM Timer1ms_Semaphore; // Ajouté par Xavier & Florian
+//RT_SEM Timer1ms_Semaphore; // Ajouté par Xavier & Florian
 
 //--------------------------------------------
 //  Events identifiers
@@ -77,7 +73,7 @@ static bool AbortExperiment_Flag;
 static bool Finished_Flag; // Ajouté par Xavier & Florian
 
 bool ExperimentIsAborted(void) { return AbortExperiment_Flag; }
-bool ConnectionIsFinished(void) { return(Finished_Flag); } // Ajouté par Xavier & Florian
+bool ExperimentIsFinished(void) { return(Finished_Flag); } // Ajouté par Xavier & Florian
 
 // les lignes suivantes ont été supprimées par Celestin & Jordan - TODO: A confirmer que c'est bien une erreur ?
 // declared in WheelHMI.c 
@@ -95,65 +91,118 @@ void HumanMachineInterface_Task()
     ManageRequest();
 }
 
+void AcquireAndLaw_Task();
+
 // Ajouté par Xavier & Florian
 void TimeManagement_Task()
 {
     rt_printf("Starting Time Management task\r\n");
+    
+    /* Turn into periodic task : 1ms */
+    rt_task_set_periodic(NULL,TM_NOW,1000000);
 
-	char AP_flag = AF_FLAG;
-	char LP_flag = LP_FLAG;
-	char I_flag = I_FLAG;
-	char F_flag = F_FLAG;
+    char AP_flag = AP_FLAG;
+    char LP_flag = LP_FLAG;
+    char I_flag = I_FLAG;
+    char F_flag = F_FLAG;
 
-    Finished_Flag = false;
+    
 
     while(true) { 
 
 
         rt_sem_p(&StartExperiment_Semaphore, TM_INFINITE);
+        Finished_Flag = false;
+        rt_task_start(&AcquireAndLaw_TaskDescriptor, &AcquireAndLaw_Task, NULL);
 
         int t=0; 
         //Push I to queue
-		rt_queue_write(&TimeManagement_Queue, &I_flag, sizeof(char), Q_NORMAL);
+	rt_queue_write(&TimeManagement_Queue, &I_flag, sizeof(char), Q_NORMAL);
 
         while(ConnectionIsActive() && t < ExperimentParameters.duration && !ExperimentIsAborted()) {
-			// Attente du semaphore du timer 1ms
-			rt_sem_p(&Timer1ms_Semaphore, 2000000); //timeout = 2ms
+            rt_task_wait_period(NULL); /* Pending the next périod in 1ms */
 
             t++;
 
             if (t % ExperimentParameters.lawPeriod == 0){ 
                 //Push LP to queue
-				rt_queue_write(&TimeManagement_Queue, &LP_flag, sizeof(char), Q_NORMAL);
+		rt_queue_write(&TimeManagement_Queue, &LP_flag, sizeof(char), Q_NORMAL);
             } 
 
             if (t % ExperimentParameters.acquisitionPeriod == 0){ 
                 //Push AP to queue 
-				rt_queue_write(&TimeManagement_Queue, &AP_flag, sizeof(char), Q_NORMAL);
+		rt_queue_write(&TimeManagement_Queue, &AP_flag, sizeof(char), Q_NORMAL);
             } 
 
         } 
 
         Finished_Flag = true;
         // Push F to queue 
-		rt_queue_write(&TimeManagement_Queue, &F_flag, sizeof(char), Q_NORMAL);
+        rt_queue_write(&TimeManagement_Queue, &F_flag, sizeof(char), Q_NORMAL);
     } 
 }
 
-void Timer1ms_Task(){
+/*void Timer1ms_Task(){
 	rt_sem_v(&Timer1ms_Semaphore);	
+}*/
+
+// Ajouté par Xavier & Florian
+void LawFunction()
+{
+    rt_printf("Law Function function\r\n");
+        
 }
 
 // Ajouté par Xavier & Florian
-void LawFunction_Task()
+void CurvesAcquisition()
 {
-    // TODO
+    rt_printf("Curves Acquisition function\r\n");
+    
 }
 
 // Ajouté par Xavier & Florian
-void CurvesAcquisition_Task()
+void Finishing()
 {
-    // TODO
+    rt_printf("Finishing function\r\n");
+    
+}
+
+
+// Ajouté par Xavier & Florian
+void Initializing()
+{
+    rt_printf("Initializing function\r\n");
+    
+}
+
+void AcquireAndLaw_Task()
+{    
+    char MsgBlock;
+    
+    rt_printf("Acquire And Law Task\r\n");
+    int i;
+    
+    while (1){
+        rt_queue_read(&TimeManagement_Queue, &MsgBlock, sizeof(char), TM_INFINITE);  
+        rt_printf("Flag %d\n\r", MsgBlock);
+
+        switch(MsgBlock){
+            case AP_FLAG: 
+                CurvesAcquisition();
+                break;
+            case LP_FLAG: 
+                LawFunction();
+                break;
+            case I_FLAG: 
+                Initializing();
+                break;
+            case F_FLAG: 
+                Finishing();
+                break;             
+        }           
+        
+    }
+    
 }
 
 
@@ -171,7 +220,8 @@ void AbortExperiment(void)
 
 void StartExperiment(void)
 {
-    AbortExperiment_Flag = False;  // Ajouté par Florian pour initialisation
+    rt_printf("StartExperiment\n\r");
+    AbortExperiment_Flag = false;  // Ajouté par Florian pour initialisation
     rt_sem_v(&StartExperiment_Semaphore);
 }
 //----------------------------------------------------------
@@ -181,16 +231,32 @@ void ReturnSensorsMeasurement()
     float samplesBlock[50 * 4];
     char terminationChar;
     int i = 0;
+    
+    RT_QUEUE_INFO queueInfo;
 
     // read from the queue
     // write into samplesBlock at the right position
     // increment i (for next iteration's position)
     // don't wait for message
     // if no messages, read returns EWOULDBLOCK, so the loop stops
-    while(rt_queue_read(&SensorsMeasurement_Queue, samplesBlock + (i*4), 4*sizeof(float), TM_NONBLOCK) != EWOULDBLOCK)
-        i++;
-
-    terminationChar = ExperimentIsFinished() ? 'S' : 'F';
+    
+    rt_queue_inquire(&SensorsMeasurement_Queue, &queueInfo);
+    for (i=0; i<queueInfo.nmessages; i++){
+        rt_queue_read(&SensorsMeasurement_Queue, samplesBlock + (i*4), 4*sizeof(float), TM_NONBLOCK);       
+    }    
+    
+   
+    // Truc de base
+    /*for(i = 0;i<10;i++)
+    {
+        samplesBlock[ (i * 4)+0] = 0.1;
+        samplesBlock[ (i * 4)+1] = 0.2;
+        samplesBlock[ (i * 4)+2] = 0.3;
+        samplesBlock[ (i * 4)+3] = 0.4;
+    }*/
+    
+    
+    terminationChar = ExperimentIsFinished() ? 'F' : 'S';
 
     WriteRealArray(terminationChar, samplesBlock, i * 4);
 }
@@ -242,7 +308,7 @@ int main(int argc, char* argv[])
     // ------------------------------------
     rt_sem_create(&ExitApplication_Semaphore, "Exit", 0, S_FIFO);
     rt_sem_create(&StartExperiment_Semaphore, "StartExperiment", 0, S_PULSE);
-    rt_sem_create(&Timer1ms_Semaphore); // Ajouté par Florian & Xavier
+    //rt_sem_create(&Timer1ms_Semaphore, "Timer1ms", 0, S_FIFO); // Ajouté par Florian & Xavier
     // **** This space must be completed  if needed   ***** 
 
 
@@ -258,21 +324,16 @@ int main(int argc, char* argv[])
     rt_task_create(&HumanMachineInterface_TaskDescriptor, "ManageRequestTask", DEFAULTSTACKSIZE, HUMAN_MACHINE_INTERFACE_PRIORITY, 0);
 
     // Ajouté par Florian & Xavier,
-	// TODO: mode=0 à modifier et à faire valider
+    // TODO: mode=0 à modifier et à faire valider
     rt_task_create(&TimeManagement_TaskDescriptor, "TimeManagement", DEFAULTSTACKSIZE, TIME_MANAGEMENT_PRIORITY, 0);
-    rt_task_create(&CurvesAcquisition_TaskDescriptor, "CurvesAcquisition", DEFAULTSTACKSIZE, CURVES_ACQUISITION_PRIORITY, 0);
-    rt_task_create(&LawFunction_TaskDescriptor, "LawFunction", DEFAULTSTACKSIZE, LAW_FUNCTION_PRIORITY, 0);
-    rt_task_create(&Timer1ms_TaskDescriptor, "Timer1ms", DEFAULTSTACKSIZE, LAW_FUNCTION_PRIORITY, 0);
+    rt_task_create(&AcquireAndLaw_TaskDescriptor, "AcquireAndLaw", DEFAULTSTACKSIZE, ACQUIRE_AND_LAW_PRIORITY, 0);
 
     // Tasks starting
     rt_task_start(&HumanMachineInterface_TaskDescriptor, &HumanMachineInterface_Task, NULL);
 
     // Ajouté par Florian & Xavier
-	rt_task_set_periodic(&Timer1ms_TaskDescriptor,TM_NOW,1000000); // activation each 1ms
-    rt_task_start(&Timer1ms_TaskDescriptor, &Timer1ms_Task, NULL);
     rt_task_start(&TimeManagement_TaskDescriptor, &TimeManagement_Task, NULL);
-    rt_task_start(&CurvesAcquisition_TaskDescriptor, &CurvesAcquisition_Task, NULL);
-    rt_task_start(&LawFunction_TaskDescriptor, &LawFunction_Task, NULL);
+    
 
     //-----------------------------------------------------------
     // Main Task waits on exit semaphore                          
@@ -288,10 +349,8 @@ int main(int argc, char* argv[])
     rt_task_delete(&HumanMachineInterface_TaskDescriptor);
 
     // Ajouté par Florian & Xavier
-    rt_task_delete(&Timer1ms_TaskDescriptor);	
     rt_task_delete(&TimeManagement_TaskDescriptor);
-    rt_task_delete(&CurvesAcquisition_TaskDescriptor);
-    rt_task_delete(&LawFunction_TaskDescriptor);
+    rt_task_delete(&AcquireAndLaw_TaskDescriptor);
 
     //------------------------------------------------------------
     // Semaphores destruction                                     
@@ -301,7 +360,6 @@ int main(int argc, char* argv[])
 
     rt_sem_delete(&ExitApplication_Semaphore);
     rt_sem_delete(&StartExperiment_Semaphore);
-    rt_sem_delete(&Timer1ms_Semaphore); // Ajouté par Florian & Xavier
 
     //------------------------------------------------------------
     // Events destruction                                 
