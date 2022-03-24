f11423e6de4d9ae5770b4ebf86d028670e865277
Grenier Célestin <43825779+PictElm@users.noreply.github.com>
2022-03-24 12:35:44 +0100

    WheelTask

--- WheelTasks.c
+++ WheelTasks.c
@@ -27,9 +27,8 @@
 //--------------------------------------------
 // Tasks descriptors
 //--------------------------------------------
-
 RT_TASK HumanMachineInterface_TaskDescriptor;
-// Ajouté par Xavier & Florian
+// Added (resp. Task 2 and Task 3)
 RT_TASK TimeManagement_TaskDescriptor;
 RT_TASK AcquireAndLaw_TaskDescriptor;
 
@@ -37,37 +36,28 @@ RT_TASK AcquireAndLaw_TaskDescriptor;
 //-------------------------------------------
 // Semaphores identifiers
 //-------------------------------------------
-// RT_SEM mySemaphore;
+// Added is waited on before starting the experiment (in TimeManagment_Task)
 RT_SEM StartExperiment_Semaphore;
+// waited on in the main function, triggers the release of resources
 RT_SEM ExitApplication_Semaphore;
-
-//--------------------------------------------
-// Events identifiers
-//--------------------------------------------
-// RT_EVENT myEvents;
-//#define BIT(n) (1<<n)
-//#define EVENT_1    BIT(0)
-//#define EVENT_2    BIT(1)
-//#define EVENT_ALL (EVENT_1 + EVENT_2)
-
-//--------------------------------------------
-// Mutual exclusion semaphores identifiers
-//--------------------------------------------
-// RT_MUTEX myMutex;
 
 //--------------------------------------------
 // Message queues Identifiers
 //--------------------------------------------
 RT_QUEUE SensorsMeasurement_Queue;
-RT_QUEUE TimeManagement_Queue; // Ajouté par Xavier & Florian
+// Added (contains flags AP_FLAG, LP_FLAG, I_FLAG and F_FLAG,
+// witten to in TimeManagment_Task and read from AcquireAndLow_Task)
+RT_QUEUE TimeManagement_Queue;
 
 // Global variables communication and synchronization tasks by shared memory
 //--------------------------------------------------------------------------
 static bool AbortExperiment_Flag;
-static bool Finished_Flag; // Ajouté par Xavier & Florian
+// Added (reset when starting the experiment,
+// set when the experiment is over or aborted)
+static bool Finished_Flag;
 
 bool ExperimentIsAborted(void) { return AbortExperiment_Flag; }
-bool ExperimentIsFinished(void) { return Finished_Flag; } // Ajouté par Xavier & Florian
+bool ExperimentIsFinished(void) { return Finished_Flag; } // Added
 
 // declared in WheelHMI.c
 extern ExperimentParametersType ExperimentParameters;
@@ -86,7 +76,7 @@ void HumanMachineInterface_Task()
 
 void AcquireAndLaw_Task();
 
-// Ajouté par Xavier & Florian
+// Added (Task 2)
 void TimeManagement_Task()
 {
     rt_printf("Starting Time Management task\r\n");
@@ -100,82 +90,94 @@ void TimeManagement_Task()
     char F_flag = F_FLAG;
 
     while (true) {
+        // wait for the experiment to be started
         rt_sem_p(&StartExperiment_Semaphore, TM_INFINITE);
         Finished_Flag = false;
+        // start Task 3
         rt_task_start(&AcquireAndLaw_TaskDescriptor, &AcquireAndLaw_Task, NULL);
 
+        // time counter thingy
         int t = 0;
-        // Push I to queue
+        // Push I to queue for Task 3 to read
         rt_queue_write(&TimeManagement_Queue, &I_flag, sizeof(char), Q_NORMAL);
 
         while (ConnectionIsActive() && t < ExperimentParameters.duration && !ExperimentIsAborted()) {
-            rt_task_wait_period(NULL); // Pending the next périod in 1ms
+            rt_task_wait_period(NULL); // Pending the next period in 1ms
 
             t++;
 
             if (t % ExperimentParameters.lawPeriod == 0) {
-                // Push LP to queue
+                // Push LP to queue for Task 3 to read
                rt_queue_write(&TimeManagement_Queue, &LP_flag, sizeof(char), Q_NORMAL);
             }
 
             if (t % ExperimentParameters.acquisitionPeriod == 0) {
-                // Push AP to queue
+                // Push AP to queue for Task 3 to read
                 rt_queue_write(&TimeManagement_Queue, &AP_flag, sizeof(char), Q_NORMAL);
             }
 
         }
 
+        // the experiment is finished
         Finished_Flag = true;
         // Push F to queue
         rt_queue_write(&TimeManagement_Queue, &F_flag, sizeof(char), Q_NORMAL);
     }
 }
 
-// Ajouté par Xavier & Florian
+// Added
 void LawFunction()
 {
+    // read the values from the sensor...
     SampleType Ploudleretour;
     SampleAcquisition(&Ploudleretour);
-    //rt_printf("motorSpeed : %f / platformSpeed : %f /  platformPosition : %f / motorCurrent : %f\r\n", Ploudleretour.motorSpeed, Ploudleretour.platformSpeed, Ploudleretour.platformPosition, Ploudleretour.motorCurrent);
 
+    // ... and forward it to the law computation
     float WheelCommand = ComputeLaw(Ploudleretour);
+    // then apply the command
     ApplySetpointCurrent(WheelCommand);
 }
 
-// Ajouté par Xavier & Florian
+// Added
 void CurvesAcquisition()
 {
+    // read the values form the sensor...
     float SensorsValues[4];
     SensorAcquisition(&SensorsValues);
+    // ... and push them into the queue for the HMI task to read
     rt_queue_write(&SensorsMeasurement_Queue, &SensorsValues, 4*sizeof(float), Q_NORMAL);
 }
 
-// Ajouté par Xavier & Florian
+// Added
 void Finishing()
 {
     rt_printf("Finishing\r\n");
-    ApplySetpointCurrent(0);
+    ApplySetpointCurrent(0); // stops the motor (command to 0)
 }
 
 
-// Ajouté par Xavier & Florian
+// Added
 void Initializing()
 {
     rt_printf("Initializing\r\n");
+    // the experiment needs to be initialize with an initial value from the sensor
     SampleType Ploudleretour;
     SampleAcquisition(&Ploudleretour);
     InitializeExperiment(Ploudleretour);
 }
 
+// Added (Task 3)
 void AcquireAndLaw_Task()
 {
-    char MsgBlock;
+    char MsgBlock; // the received action to do (from Task 2)
 
     rt_printf("Acquire And Law Task\r\n");
 
     while (1) {
+        // wait for something to do
         rt_queue_read(&TimeManagement_Queue, &MsgBlock, sizeof(char), TM_INFINITE);
 
+        // just do it
         switch (MsgBlock) {
             case AP_FLAG:
                 CurvesAcquisition();
@@ -209,11 +211,13 @@ void AbortExperiment(void)
 void StartExperiment(void)
 {
     rt_printf("Starting Experiment\n\r");
-    AbortExperiment_Flag = false; // Ajouté par Florian pour initialisation
+    AbortExperiment_Flag = false; // Added (reset experiment flag)
     rt_sem_v(&StartExperiment_Semaphore);
 }
 //----------------------------------------------------------
 
+// Modified
+// is called form the HMI when the ground station asks for the measurments
 void ReturnSensorsMeasurement()
 {
     float samplesBlock[50 * 4];
@@ -222,20 +226,13 @@ void ReturnSensorsMeasurement()
 
     RT_QUEUE_INFO queueInfo;
 
+    // read everything in the queue
     rt_queue_inquire(&SensorsMeasurement_Queue, &queueInfo);
     for (i = 0; i < queueInfo.nmessages; i++) {
         rt_queue_read(&SensorsMeasurement_Queue, samplesBlock + (i*4), 4*sizeof(float), TM_NONBLOCK);
     }
 
-    // Truc de base
-    /*for(i = 0;i<10;i++)
-    {
-        samplesBlock[ (i * 4)+0] = 0.1;
-        samplesBlock[ (i * 4)+1] = 0.2;
-        samplesBlock[ (i * 4)+2] = 0.3;
-        samplesBlock[ (i * 4)+3] = 0.4;
-    }*/
-
+    // send (the 'F' flag is to be used only when the experiment is over)
     terminationChar = ExperimentIsFinished() ? 'F' : 'S';
     WriteRealArray(terminationChar, samplesBlock, i * 4);
 }
@@ -271,9 +268,6 @@ int main(int argc, char* argv[])
     // -------------------------------
 
     // -------------------------------------
-    // Events creation
-    // rt_event_create(xxxx);
-
     // Message queues creation
     // -------------------------------------
     rt_queue_create(&SensorsMeasurement_Queue, "SensorsMeasurement_Queue", 50 * 4*sizeof(float), 50, Q_FIFO);
@@ -284,24 +278,19 @@ int main(int argc, char* argv[])
     rt_sem_create(&ExitApplication_Semaphore, "Exit", 0, S_FIFO);
     rt_sem_create(&StartExperiment_Semaphore, "StartExperiment", 0, S_PULSE);
 
-    // Mutual exclusion semaphore creation
-    //---------------------------------------------------------
-    // rt_mutex_create(&myMutex, "mx");
-
     // Tasks creation
     //---------------------------------------------------------
 
     rt_task_create(&HumanMachineInterface_TaskDescriptor, "ManageRequestTask", DEFAULTSTACKSIZE, HUMAN_MACHINE_INTERFACE_PRIORITY, 0);
 
-    // Ajouté par Florian & Xavier,
-    // TODO: mode=0 à modifier et à faire valider
+    // Added
     rt_task_create(&TimeManagement_TaskDescriptor, "TimeManagement", DEFAULTSTACKSIZE, TIME_MANAGEMENT_PRIORITY, 0);
     rt_task_create(&AcquireAndLaw_TaskDescriptor, "AcquireAndLaw", DEFAULTSTACKSIZE, ACQUIRE_AND_LAW_PRIORITY, 0);
 
     // Tasks starting
     rt_task_start(&HumanMachineInterface_TaskDescriptor, &HumanMachineInterface_Task, NULL);
 
-    // Ajouté par Florian & Xavier
+    // Added
     rt_task_start(&TimeManagement_TaskDescriptor, &TimeManagement_Task, NULL);
 
     //-----------------------------------------------------------
@@ -316,7 +305,7 @@ int main(int argc, char* argv[])
     //-----------------------------------------------------------
     rt_task_delete(&HumanMachineInterface_TaskDescriptor);
 
-    // Ajouté par Florian & Xavier
+    // Added
     rt_task_delete(&TimeManagement_TaskDescriptor);
     rt_task_delete(&AcquireAndLaw_TaskDescriptor);
 
@@ -335,7 +324,7 @@ int main(int argc, char* argv[])
     // Message queues destruction
     //------------------------------------------------------------
     rt_queue_delete(&SensorsMeasurement_Queue);
-    rt_queue_delete(&TimeManagement_Queue); // Ajouté par Florian & Xavier
+    rt_queue_delete(&TimeManagement_Queue); // Added
 
     rt_printf(" Application ..... finished--> exit\r\n");
     // Peripherals uninitialization
